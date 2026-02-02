f00939a36660215c39d3419bef1bfc0e31855434
DNKYr <dnkyr2007@gmail.com>
2026-02-01 16:31:40 -0800

    using nixpkgs unstable branch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     
     inputs = {
         # Adding package source for nix packages
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
 	home-manager = {
 	    url = "github:nix-community/home-manager/release-25.11";
