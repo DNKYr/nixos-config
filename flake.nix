6857148a9e46574e0029e89e4a58b94a0788ac99
DNKYr <dnkyr2007@gmail.com>
2026-02-09 09:44:18 -0800

    ADD: Automatically backup previous config

--- flake.nix
+++ flake.nix
@@ -1,38 +1,42 @@
 {
-    description = "DNKYr's nix flake";
-    
-    inputs = {
-        # Adding package source for nix packages
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "DNKYr's nix flake";
 
-	home-manager = {
-	    # Make home-manager set to the master branch
-	    url = "github:nix-community/home-manager";
-	    inputs.nixpkgs.follows = "nixpkgs";
-	};
+  inputs = {
+    # Adding package source for nix packages
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      # Make home-manager set to the master branch
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = inputs@{ nixpkgs, home-manager, ...}: {
-	nixosConfigurations = {
-	    aether = nixpkgs.lib.nixosSystem{
-	        modules = [
-		    # Import disk optimize file
-		    ./optimizeStore.nix
+  outputs =
+    inputs@{ nixpkgs, home-manager, ... }:
+    {
+      nixosConfigurations = {
+        aether = nixpkgs.lib.nixosSystem {
+          modules = [
+            # Import disk optimize file
+            ./optimizeStore.nix
 
-		    # Import previous configuration
-		    ./configuration.nix
+            # Import previous configuration
+            ./configuration.nix
 
-		    # Add home-manager module
-		    home-manager.nixosModules.home-manager
-		    {
-		        home-manager.useGlobalPkgs = true;
-		        home-manager.useUserPackages = true;
-
-		        home-manager.users.dnkyr = import ./home/home.nix;
-		    }
-	        ];
-	    };
-	};
+            # Add home-manager module
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.dnkyr = import ./home/home.nix;
+                backupFileExtension = "backup";
+              };
+            }
+          ];
+        };
+      };
     };
 
 }
