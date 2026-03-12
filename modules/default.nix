dfc8a2263aa871d2b646ce592dcd8844a4a1f70c
DNKYr <dnkyr2007@gmail.com>
2026-03-12 09:31:20 -0700

    ADD(gaming): Steam and Gamemode

--- modules/default.nix
+++ modules/default.nix
@@ -4,5 +4,6 @@
   imports = [
     ./niri.nix
     ./fonts
+    ./gaming.nix
   ];
 }
---
+++ modules/gaming.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = false;
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  # 32-bit graphics support required for Proton
+  hardware.graphics.enable32Bit = true;
+}
