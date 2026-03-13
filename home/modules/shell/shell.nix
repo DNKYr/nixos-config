efa75f6dd378102f96035217fe466e72cbc7ad4b
DNKYr <dnkyr2007@gmail.com>
2026-03-13 09:28:33 -0700

    MODIFY(nushell): Remove welcome banner nushell and add completion

--- home/modules/shell/shell.nix
+++ home/modules/shell/shell.nix
@@ -30,6 +30,14 @@
 
   programs.nushell = {
     enable = true;
+    settings = {
+      show_banner = false;
+
+      completions.external = {
+        enable = true;
+        max_results = 200;
+      };
+    };
   };
 
   programs.starship = {
