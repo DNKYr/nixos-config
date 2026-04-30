53b1b2c1e1650be8c1ca08035805fbaa37e8af3b
DNKYr <dnkyr2007@gmail.com>
2026-04-30 11:38:17 -0700

    update: keep the kernel version to the latest

--- modules/hosts/aether/configuration.nix
+++ modules/hosts/aether/configuration.nix
@@ -17,6 +17,8 @@
 
       ];
 
+      boot.kernelPackages = pkgs.linuxPackages_latest;
+
       # Bootloader.
       boot.loader.systemd-boot.enable = false;
       boot.loader.efi.canTouchEfiVariables = true;
