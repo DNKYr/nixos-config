7f12d3c6223e5b24dac895054e3cb42b3d2ad471
DNKYr <dnkyr2007@gmail.com>
2026-05-08 08:24:39 -0700

    feat: add dw-proton for Arknights:Endfield

--- home/modules/gui/gaming.nix
+++ home/modules/gui/gaming.nix
@@ -10,5 +10,6 @@
     inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin
     pkgs.heroic
     pkgs.prismlauncher # Minecraft Launcher
+    pkgs.protonplus # DW-Proton for Arknights:Endfield
   ];
 }
