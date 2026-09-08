7f90b5fa639d87c7d2f7bc0a2b325960d58f39df
DNKYr <dnkyr2007@gmail.com>
2026-09-08 17:09:52 +0800

    fix(nvim): commit lazy-lock.json and fix conform load events

--- configs/nvim/lua/plugins/formatting.lua
+++ configs/nvim/lua/plugins/formatting.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = { "BufWritePre" },
+    event = { "BufWritePre", "BufReadPost", "BufNewFile" },
     opts = {
       formatters_by_ft = {
         nix = { "nixpkgs_fmt" },
